e6c0377446e2efcdac4037639b6738ff532d9c33
avyukd <avyukd@gmail.com>
2017-03-20 22:12:08 -0400

    Day 2
    Added a tilt class for the tilt of the robot (does not work completely
    yet)
    Everything other than the compass and tilt is done except for ArmPanel
    and the shortcut window, which are pretty easy.
    Java to Ardunio Interface? Text files can work for now.
    MotorPanel demonstrates

--- GUIDriver.java
+++ GUIDriver.java
@@ -1,10 +1,10 @@
 	import javax.swing.JFrame;
 public class GUIDriver {
  		public static void main(String[] args){
-			JFrame frame = new JFrame("Motor Panel Test");
-			frame.setSize(400, 150);
+			JFrame frame = new JFrame("Tilt Panel Test");
+			frame.setSize(400, 400);
 			frame.setLocation(100,50);
-			frame.setContentPane(new LightsPanel());
+			frame.setContentPane(new TiltPanel());
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 		}
